0d76031ca4bcf6506f0541c56a21b638a6e2e227
Wesley Miranda <wesleywes42@gmail.com>
2022-06-12 09:55:45 -0600

    Date format

---
+++ lib/generator.rb
@@ -0,0 +1,26 @@
+require "pry"
+require 'date'
+class Generator
+  attr_reader :a_z, :random
+
+  def initialize
+    @a_z = ("a".."z").to_a << " "
+    @random = (0...5).map {rand(0...9)}.join.to_i
+    @date = Time.new
+  end
+
+  def date_to_format
+      @date.strftime("%d%m%y").to_i
+  end
+
+  def date_squared
+    date_to_format * date_to_format
+  end
+
+
+
+
+
+
+
+end
