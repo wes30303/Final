e451ea48411db48d8a0e7cc7019ea99884d0ec63
Wesley Miranda <wesleywes42@gmail.com>
2022-06-13 13:41:07 -0600

    encrypt fix

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,6 +5,6 @@ input = ARGV[0]
 output = ARGV[1]
 encryption = enigma.encrypt(File.read(input).tr("\n", ""), "02715", "040895")
 File.open(output, "w") do |file|
-  file.read encryption[:encryption]
+  file.write encryption[:encryption]
   puts "create #{output} with the key #{encryption[:key]} and date #{encryption[:date]}"
 end
