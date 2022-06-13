a46b4c445787216323080b547372fbc5fecefa50
Wesley Miranda <wesleywes42@gmail.com>
2022-06-12 20:20:31 -0600

    added deleted things

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -15,16 +15,34 @@ RSpec.describe Generator do
     expect(@generator.keys.first).to be_a Numeric
   end
 
+  it "has a date to be sqared" do
+    expect(@generator.date_squared).to eq(1025)
+  end
+
+  it "can be a date" do
+    expect(@generator.date).to eq("040895")
+  end
+
+  it "can do a random number" do
+    expect(@generator.random_num).to be_a Integer
+  end
+
   it "can show the shifts" do
     expect(@generator.shifts.first).to be_a Numeric
   end
-
 
   it "can rotate hello world" do
     expect(@generator.rotate_a).to be_a Hash
     expect(@generator.rotate_b).to be_a Hash
     expect(@generator.rotate_c).to be_a Hash
     expect(@generator.rotate_d).to be_a Hash
+  end
+
+  it "can reverse keder ohulw rotate " do
+    expect(@generator.reverse_rotate_a).to be_a Hash
+    expect(@generator.reverse_rotate_b).to be_a Hash
+    expect(@generator.reverse_rotate_c).to be_a Hash
+    expect(@generator.reverse_rotate_d).to be_a Hash
   end
 
   it "is able to return a hash" do
@@ -41,5 +59,5 @@ RSpec.describe Generator do
       key: "02715",
       date: "040895"
    })
-  end 
+  end
 end
